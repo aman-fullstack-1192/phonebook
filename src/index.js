1d8147f2eed2e2f88eb7cefc960d6679a0e80d11
Aman kumar <aman@Amans-MacBook-Air.local>
2019-07-24 23:35:18 +0530

    Added state and add Subscriber page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './common/common.css'
 import App from './App';
+import AddSubscriber from './AddSubscriber';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
